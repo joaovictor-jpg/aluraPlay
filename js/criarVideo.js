924ad07922e6d2082e30f60a5447055e328c1a15
João Victor <joaovictormdasilva676@gmail.com>
2024-08-07 21:42:06 -0300

    Corrigindo import.

--- js/criarVideo.js
+++ js/criarVideo.js
@@ -1,12 +1,18 @@
+import { connectaApi } from "./connectaApi.js";
+
 const formulario = document.querySelector('[data-formulario]');
 
-function criarVideo(evento) {
+async function criarVideo(evento) {
     evento.preventDefault();
-    
+
     const url = document.querySelector('[data-url]').value;
-    const titulo = documet.querySelector('[data-titulo]').value;
+    const titulo = document.querySelector('[data-titulo]').value;
     const imagem = document.querySelector('[data-imagem]').value;
     const descricao = Math.floor(Math.random() * 10).toString();
+
+    await connectaApi.criaVideo(titulo, descricao, url, imagem);
+
+    window.location.href = '../pages/envio-concluido.html';
 
 }
 
